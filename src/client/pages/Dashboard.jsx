5279ed92512034d58ded93333ca9219fda179f09
Parth Vaishnani <parth.vaishnani@lntinfotech.com>
2023-06-24 13:28:04 +0530

    commit for fetch api

--- src/client/pages/Dashboard.jsx
+++ src/client/pages/Dashboard.jsx
@@ -1,4 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import Axios from "axios";
 import TaskList from "./redux-pages/TaskLists";
 import Task from "./redux-pages/Tasks";
 import { userLogout } from "../redux-store/redux-toolkit-code/slices/userSlice";
@@ -15,6 +17,15 @@ function Dashboard() {
       dispatch(userLogout());
     }
   };
+  const loadMasterData = async () => {
+    const data = await Axios.get("/api/user");
+    console.log("data", data.data.data);
+  };
+  useEffect(() => {
+    console.log("hi ");
+    loadMasterData();
+    // createData({ id: 3, name: "julubar" });
+  }, []);
   return (
     <div>
       <button
